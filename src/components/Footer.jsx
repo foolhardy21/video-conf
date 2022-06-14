035eee1e9cdb70073899383bcd87f03380b25a1a
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-06-14 19:22:08 +0530

    feat - footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
-import { useAVToggle } from "@100mslive/react-sdk";
+import { useAVToggle, useHMSActions } from "@100mslive/react-sdk";
+import styles from './footer.module.css'
 
 function Footer() {
     const {
@@ -7,13 +8,26 @@ function Footer() {
         toggleAudio,
         toggleVideo
     } = useAVToggle();
+    const hmsActions = useHMSActions();
+
+    function handleLeaveClick() {
+        hmsActions.leave()
+    }
+
     return (
-        <div className="control-bar">
-            <button className="btn-control" onClick={toggleAudio}>
-                {isLocalAudioEnabled ? "Mute" : "Unmute"}
+        <div className={`pos-fixed ${styles.footer} flx flx-center bg-secondary pd-md`}>
+            <button className="btn-txt flx flx-center pd-xs mg-right-s" onClick={toggleAudio}>
+                <span className="material-icons icon-secondary">
+                    {isLocalAudioEnabled ? "mic" : "mic_off"}
+                </span>
             </button>
-            <button className="btn-control" onClick={toggleVideo}>
-                {isLocalVideoEnabled ? "Hide" : "Unhide"}
+            <button className="btn-txt flx flx-center pd-xs" onClick={toggleVideo}>
+                <span className="material-icons icon-secondary">
+                    {isLocalVideoEnabled ? "videocam" : "videocam_off"}
+                </span>
+            </button>
+            <button onClick={handleLeaveClick} className={`btn-outlined b-solid  txt-md txt-lcase txt-err mg-left-xxlg pd-xs ${styles.btnLeave}`}>
+                leave
             </button>
         </div>
     );
